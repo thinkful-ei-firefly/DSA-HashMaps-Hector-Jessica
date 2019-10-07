c403b881ef7171a6a031bc7b694e42ebf859d72f
Hector Valles <hmvalles75@yahoo.com>
2019-10-07 11:13:11 -0700

    4

--- hashmapDrills.js
+++ hashmapDrills.js
@@ -1,7 +1,7 @@
-const Hashmaps = require('./hashmap');
+const HashMap = require('./hashmap');
 
 function main() {
-  const LOR = new Hashmaps();
+  const LOR = new HashMap();
 
   LOR.set('Hobbit', 'Bilbo');
   LOR.set('Hobbit', 'Frodo');
@@ -23,4 +23,48 @@ function main() {
 }
 
 main();
-// Two of the items that were set were overridden with tow other items with identical keys
+
+// 1.
+// Two of the items that were set were overwritten with tow other items with identical keys
+// Values we got were Sauron and Frodo. Bilbo and the Necromancer were overwritten by Sauron and Frodo
+// 24 because it gets scaled up once we get four items in the table.
+
+const WhatDoesThisDo = function() {
+  let str1 = 'Hello World.';
+  let str2 = 'Hello World.';
+  let map1 = new HashMap();
+  map1.set(str1, 10);
+  map1.set(str2, 20);
+  let map2 = new HashMap();
+  let str3 = str1;
+  let str4 = str2;
+  map2.set(str3, 20);
+  map2.set(str4, 10);
+
+  console.log(map1.get(str1));
+  console.log(map2.get(str3));
+};
+//WhatDoesThisDo();
+
+// We defined two identical strings and passed them as keys to a new hashmap map 1,
+// because the keys were identical the second set of value overrides the first set,
+// We then do the same exact thing on map 2. Then we console.log the values in each map.
+
+function removeDuplicates(string) {
+  let hashMap = new Map();
+  let result = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (!hashMap.has(string[i])) {
+      hashMap.set(string[i], '');
+    }
+  }
+
+  for (let [key, value] of hashMap) {
+    result += key;
+  }
+  return result;
+}
+
+console.log(removeDuplicates('google'));
+console.log(removeDuplicates('google all that you can think of'));
